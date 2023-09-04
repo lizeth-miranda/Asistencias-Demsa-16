c6b4000afcf598d4ae7b0b5325a0bcc08b669baf
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2023-09-04 17:29:24 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Asistencias Demsa16',
-    'version': '16.4',
+    'version': '16.5',
     'author': 'Demsa Industrial',
     'website': '',
     'depends': [
